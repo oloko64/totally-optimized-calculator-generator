85ea217e512f70fb0abbd013ca81819f8e0b7f66
OLoKo64 <naldoepingo1@gmail.com>
2022-07-26 00:20:34 -0300

    feat: add concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod utils;
 
-use std::fs;
+use rayon::prelude::*;
+use std::{fs, sync::Mutex};
 
 fn main() {
     // You can set the file name here.
@@ -9,7 +10,6 @@ fn main() {
     // You can set the maximum number that the calculator can process here, but I don't recommend you to go over 100, it will crash when you try to open the generated file. :)
     // If you use a value of 1000 it will generate a file with +8mi lines and around 200MB.
     let maximum_number = 100;
-
 
     // ##############################################################################
     // ############################## DON'T EDIT ####################################
@@ -23,21 +23,25 @@ fn create_header(file: &str) {
 }
 
 fn create_body(file: &str, max: i32) {
-    let mut file = fs::OpenOptions::new()
-        .write(true)
-        .append(true) // This is needed to append to file
-        .open(file)
-        .unwrap();
+    let file = Mutex::from(
+        fs::OpenOptions::new()
+            .write(true)
+            .append(true) // This is needed to append to file
+            .open(file)
+            .unwrap(),
+    );
 
     for op in ['+', '-', '*', '/'].iter() {
-        let mut n2 = 0;
-        while n2 <= max {
-            for (n1, _) in (0..max + 1).enumerate() {
-                let res: utils::NumType = utils::calc_result(n1.try_into().unwrap(), n2, op);
-                utils::write_format_line(&mut file, n1.try_into().unwrap(), n2, op, &res);
-            }
-            n2 += 1;
-        }
+        (0..=max).into_par_iter().for_each(|n2| {
+            (0..max + 1)
+                .into_par_iter()
+                .enumerate()
+                .for_each(|(n1, _)| {
+                    let res: utils::NumType = utils::calc_result(n1.try_into().unwrap(), n2, op);
+                    let mut guard = file.lock().unwrap();
+                    utils::write_format_line(&mut guard, n1.try_into().unwrap(), n2, op, &res);
+                });
+        });
     }
     drop(file);
 }
