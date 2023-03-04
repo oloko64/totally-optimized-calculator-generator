ca81b0348268416ea2d659a989bfe58c44e1462d
OLoKo64 <naldoepingo1@gmail.com>
2023-03-04 01:56:42 -0300

    feat: add clap and remove rayon

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,41 @@
 mod utils;
 
-use rayon::prelude::*;
-use std::env;
 use std::error::Error;
 use std::fs;
 use std::io::{BufWriter, Write};
-use std::sync::Mutex;
 use std::time::Instant;
 
-fn main() {
+use clap::Parser;
+
+/// This is a calculator that generates a python file that can calculate any number from 0 to the maximum number you specify.
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// The file name to be generated.
+    #[arg(short, long, default_value = "calc.py")]
+    file: String,
+
+    /// The maximum number that the calculator can process. (don't use over 3000)
+    #[arg(short, long, default_value = "100")]
+    max: u32,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
     let time = Instant::now();
-    // You can set the file name here.
-    let file = "calc.py";
-
-    // You can set the maximum number that the calculator can process here, but I don't recommend you to go over 100, it will crash when you try to open the generated file. :)
-    // If you use a value of 1000 it will generate a file with +8mi lines and around 200MB.
-    let default_maximum_number: u32 = 100;
-
-    // ##############################################################################
-    // ############################## DON'T EDIT ####################################
-    // ##############################################################################
-    let args = env::args().collect::<Vec<_>>();
-    let maximum_number = if let Ok(arg) = convert_max_to_u32(&args) {
-        println!("\nUsing the maximum number of \"{arg}\"\n");
-        arg
-    } else {
-        println!(
-                     "\nThe maximum number is not a valid number, using the default value of: \"{default_maximum_number}\"\n"
-                 );
-        default_maximum_number
-    };
-    create_header(file);
-    create_body(file, maximum_number);
+    create_header(&args.file)?;
+    create_body(args.file, args.max)?;
     println!(
         "Time taken to create file: {:.4}s",
         time.elapsed().as_secs_f32()
     );
+    Ok(())
 }
 
-fn convert_max_to_u32(args: &Vec<String>) -> Result<u32, Box<dyn Error>> {
-    if args.len() < 2 {
-        return Err(From::from("The maximum number is not specified"));
-    }
-    Ok(args[1].parse::<u32>()?)
-}
-
-fn create_header(file: &str) {
+fn create_header<T>(file: T) -> Result<(), Box<dyn Error>>
+where
+    T: AsRef<str>,
+{
     let header = r#"
 print("Welcome to the calculator MK I")
 num1 = input("Insert the first number: ")
@@ -54,25 +44,29 @@ num2 = input("Insert the second number: ")
 num1 = int(num1)
 num2 = int(num2)
 "#;
-    fs::write(file, header).expect("Unable to write to file");
+    fs::write(file.as_ref(), header)?;
+    Ok(())
 }
 
-fn create_body(file: &str, max: u32) {
-    let file = Mutex::new(BufWriter::new(
+fn create_body<T>(file: T, max: u32) -> Result<(), Box<dyn Error>>
+where
+    T: AsRef<str>,
+{
+    let mut file = BufWriter::new(
         fs::OpenOptions::new()
             .write(true)
             .append(true) // This is needed to append to file
-            .open(file)
-            .unwrap(),
-    ));
+            .open(file.as_ref())?,
+    );
 
-    ['+', '-', '*', '/'].par_iter().for_each(|op| {
+    for op in ['+', '-', '*', '/'] {
         for n2 in 0..=max {
             for n1 in 0..=max {
-                let res = utils::calc_result(n1, n2, *op);
-                file.lock().unwrap().write_all(res.as_bytes()).unwrap();
+                let res = utils::calc_result(n1, n2, op);
+                file.write_all(res.as_bytes())?;
             }
         }
-        file.lock().unwrap().flush().unwrap();
-    });
+        file.flush()?;
+    }
+    Ok(())
 }
